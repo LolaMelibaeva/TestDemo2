1cfd6ed577d937a4d98f5b0c4035e49e55cfd30d
LolaMelibaeva <lolam79@gmail.com>
2019-06-28 10:54:03 -0400

    new changes

--- src/com/class01/Homework2.java
+++ src/com/class01/Homework2.java
@@ -4,6 +4,8 @@ public class Homework2 {
 	public static void main(String[] args) {
 		
 		System.out.println("This is my 1st line demo");
+		
+		System.out.println("This is my 3rd line demo");
 	}
 
 }
